473ec20a4ea9751d79a5feed014a646f2b32870a
DRV111 <xblackcrownx@gmail.com>
2022-07-25 19:24:13 +0300

    feat: add some solutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -226,8 +226,14 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const newArr = [];
+  const total = arr.reduce((sum, b) => {
+    newArr.push(sum);
+    return sum + b;
+  });
+  newArr.push(total);
+  return newArr;
 }
 
 /**
